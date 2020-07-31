87cca6ed99191810e25f3adbbb5e60999c98016a
Jhonny Quispe <jhonny.quispejl@gmail.com>
2020-07-31 17:16:03 -0500

    Add default port

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const storage = multer.diskStorage({
 const app = express()
 
 // settings
-app.set('port', 3000)
+app.set('port', process.env.PORT || 3000)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
